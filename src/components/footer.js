f969969f48db425a225f0ab2beb8d00f7bd25635
Messa Ismael <messaismael01@gmail.com>
2021-02-10 16:17:15 +0100

    responsive footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,29 +1,32 @@
 import '../css/footer.css'
+import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons' 
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 
 function Footer() {
     return (
         <div>
-            <Row className='footer-main bg-dark '>
-                <Col xs={12} md={7} className="part1 text-light">
-                    <span className="span1"> Fashion Style </span>
-                    <span className="span2"> Copyright 2020 - 2021 - All rights reserved</span>
-                </Col>
-                <Col xs={12} md={5} className="part2 text-light">
-                    <a href="#FacebookAccount"> <FontAwesomeIcon icon={faFacebook} /> </a>
-                    <a href="#TwitterAccount"> <FontAwesomeIcon icon={faTwitter} /> </a>
-                    <a href="#InstagramAccount"> <FontAwesomeIcon icon={faInstagram} /> </a>
-                    <div className="vert-line"></div>
-                    <a href="#1"> Endorsement </a>
-                    <a href="#2"> FAQ </a>
-                    <a href="#3"> Contact </a>
-                    <a href="#4"> Legals Mentions </a>
-                </Col>
-            </Row>
+            <Container fluid>
+                <Row className='footer-main bg-dark '>
+                    <Col xs={12} md={6} lg={7} className="part1 text-light">
+                        <span className="span1"> Fashion Style </span>
+                        <span className="span2"> Copyright 2020 - 2021 - All rights reserved</span>
+                    </Col>
+                    <Col xs={12} md={6} lg={5} className="part2 text-light">
+                        <a href="#FacebookAccount"> <FontAwesomeIcon icon={faFacebook} /> </a>
+                        <a href="#TwitterAccount"> <FontAwesomeIcon icon={faTwitter} /> </a>
+                        <a href="#InstagramAccount"> <FontAwesomeIcon icon={faInstagram} /> </a>
+                        <div className="vert-line"></div>
+                        <a href="#1"> Endorsement </a>
+                        <a href="#2"> FAQ </a>
+                        <a href="#3"> Contact </a>
+                        <a href="#4"> Legals Mentions </a>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
