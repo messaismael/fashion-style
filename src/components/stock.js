f519dc863b97c41b3c139184f7895913f25cb43d
Messa Ismael <messaismael01@gmail.com>
2021-02-16 17:24:27 +0100

    customizing database

--- src/components/stock.js
+++ src/components/stock.js
@@ -1,46 +1,58 @@
 
-const homeData = [
-  {
-    id: "0ld0qvru-d93b-11e8-9f8b-f2801f1b9fd1",
+const homeData = {
+  section1:
+    [
+      {
+        id: "0ld0qvru-d93b-11e8-9f8b-f2801f1b9fd1",
+        category: "Shoes",
+        cover: "https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        name: "Aire NIke",
+        price: 10.95
+      },
+      {
+        id: "0o6r76vt-d93b-11e8-9f8b-f2801f1b9fd1",
+        category: "Suit men",
+        cover: "https://www.ledetaillant.biz/wp-content/uploads/2020/12/costume-homme-en-noir.jpg",
+        name: "Six button men's suit",
+        price: 50.99
+      },
+      {
+        id: "0o6r76vt-d93b-11e8-9f8b-f2801f1b9fd1",
+        category: "Dress",
+        cover: "https://www.ledetaillant.biz/wp-content/uploads/2018/10/Untitled-17-683x1024.jpg",
+        name: "Sexy blue puff sleeve mini dress",
+        price: 25.99
+      },
+    ],
+  section2: {
+    id: "0o6r76vt-d93b-11e8-9f8b-f2801f1b9fd1",
     category: "Shoes",
-    cover: "https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    name: "Aire NIke",
-    price: 10.95
-  },
-  {
-    id: "0o6r76vt-d93b-11e8-9f8b-f2801f1b9fd1",
-    category: "Suit men",
-    cover: "https://www.ledetaillant.biz/wp-content/uploads/2020/12/costume-homme-en-noir.jpg",
-    name: "Six button men's suit",
-    price: 50.99
-  },
-  {
-    id: "0o6r76vt-d93b-11e8-9f8b-f2801f1b9fd1",
-    category: "Robes",
-    cover: "https://www.ledetaillant.biz/wp-content/uploads/2018/10/Untitled-17-683x1024.jpg",
-    name: "Sexy blue puff sleeve mini dress",
+    cover: "https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80",
+    name: "Nike Free",
     price: 25.99
-  },
-]
+  }
+}
+
+
 const heroData = [
   {
     id: "0ld0qvru-d93b-11e8-9f8b-f2801f1b9fd1",
-    category: "Chaussures",
-    cover: "https://image.shutterstock.com/image-photo/blue-shoes-on-guy-studio-260nw-600199742.jpg",
+    category: "Shoes",
+    cover: "https://images.unsplash.com/photo-1533867617858-e7b97e060509?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mzh8fHNob2VzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
     name: "Blue shoes on a guy",
     price: 10.95
   },
   {
     id: "0o6r76vt-d93b-11e8-9f8b-f2801f1b9fd1",
-    category: "Robes",
-    cover: "https://img.freepik.com/free-vector/set-fashion-model-character_1308-26180.jpg?size=626&ext=jpg&ga=GA1.2.59563864.1607610129",
+    category: "Dress",
+    cover: "https://images.unsplash.com/photo-1602809152986-301c078f5fc0?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDIwfHxkcmVzc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
     name: "Set of fashion model character Free Vector",
     price: 5.99
   },
   {
     id: "0u96q42u-d93b-11e8-9f8b-f2801f1b9fd1",
     category: "Suit men",
-    cover: "https://thumbs.dreamstime.com/b/side-view-cool-dark-skinned-man-stylish-suit-touching-his-black-hat-looking-down-closeup-portrait-fashionable-african-170585815.jpg",
+    cover: "https://images.unsplash.com/photo-1481608726045-7407244fb7b6?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjAzfHxtZW4lMjBzdWl0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
     name: "Cool african american man in black hat on dark background",
     price: 23.99
   }
@@ -50,14 +62,14 @@ const heroData = [
 const stockData = [
   {
     id: "0ld0qvru-d93b-11e8-9f8b-f2801f1b9fd1",
-    category: "Chaussures",
+    category: "Shoes",
     cover: "https://www.ledetaillant.biz/wp-content/uploads/2021/01/21ed2dec-16a4-4a61-b916-1b590a9ab32f-683x1024.jpg",
     name: "Fashion style",
     price: 10.95
   },
   {
     id: "0o6r76vt-d93b-11e8-9f8b-f2801f1b9fd1",
-    category: "Robes",
+    category: "Dress",
     cover: "https://www.ledetaillant.biz/wp-content/uploads/2018/09/robe-6-683x1024.jpg",
     name: "Fashion style",
     price: 5.99
@@ -71,14 +83,14 @@ const stockData = [
   },
   {
     id: "2rb37qw5-d93b-11e8-9f8b-f2801f1b9fd1",
-    category: "Chaussures",
+    category: "Shoes",
     cover: "https://www.ledetaillant.biz/wp-content/uploads/2021/01/SLIPPER-Homme-en-cuir-marron-deux-pompons.png",
     name: "Fashion style",
     price: 12.96
   },
   {
     id: "3k9zka2c-d93b-11e8-9f8b-f2801f1b9fd1",
-    category: "Robes",
+    category: "Dress",
     cover: "https://www.ledetaillant.biz/wp-content/uploads/2018/06/robe4.jpg",
     name: "Fashion style",
     price: 65.00
@@ -92,14 +104,14 @@ const stockData = [
   },
   {
     id: "5oekg7gl-d93b-11e8-9f8b-f2801f1b9fd1",
-    category: "Chaussures",
+    category: "Shoes",
     cover: "https://www.ledetaillant.biz/wp-content/uploads/2021/01/Tennis-homme-en-bleu-blanc-rouge.png",
     name: "Fashion style",
     price: 10
   },
   {
     id: "623gsnj1-d93b-11e8-9f8b-f2801f1b9fd1",
-    category: "Robes",
+    category: "Dress",
     cover: "https://www.ledetaillant.biz/wp-content/uploads/2021/01/Robe-femme-evasee-black-and-white.png",
     name: "Fashion style",
     price: 49
@@ -113,14 +125,14 @@ const stockData = [
   },
   {
     id: "917h7iji-d93b-11e8-9f8b-f2801f1b9fd1",
-    category: "Chaussures",
+    category: "Shoes",
     cover: "https://www.ledetaillant.biz/wp-content/uploads/2021/01/Chaussure-homme-chic-en-deux-tons.png",
     name: "Fashion style",
     price: 19.99
   },
   {
     id: "9vp96t5a-d93b-11e8-9f8b-f2801f1b9fd1",
-    category: "Robes",
+    category: "Dress",
     cover: "https://www.ledetaillant.biz/wp-content/uploads/2018/10/Untitled-17-683x1024.jpg",
     name: "Fashion style",
     price: 55.99
@@ -134,14 +146,14 @@ const stockData = [
   },
   {
     id: "o3ahe30e-d93b-11e8-9f8b-f2801f1b9fd1",
-    category: "Chaussures",
+    category: "Shoes",
     cover: "https://www.ledetaillant.biz/wp-content/uploads/2021/01/Chaussure-deux-pompons-homme-de-luxe.png",
     name: "Fashion style",
     price: 55.96
   },
   {
     id: "q8qfaonc-d93b-11e8-9f8b-f2801f1b9fd1",
-    category: "Robes",
+    category: "Dress",
     cover: "https://www.ledetaillant.biz/wp-content/uploads/2018/09/Untitled-3-683x1024.jpg",
     name: "Fashion style",
     price: 80.99
@@ -155,14 +167,14 @@ const stockData = [
   },
   {
     id: "wh9yiu5w-d93b-11e8-9f8b-f2801f1b9fd1",
-    category: "Chaussures",
+    category: "Shoes",
     cover: "https://www.ledetaillant.biz/wp-content/uploads/2021/01/Chaussure-homme-en-noir-deux-tons.png",
     name: "Fashion style",
     price: 28.96
   },
   {
     id: "xfpwqd2u-d93b-11e8-9f8b-f2801f1b9fd1",
-    category: "Robes",
+    category: "Dress",
     cover: "https://www.ledetaillant.biz/wp-content/uploads/2018/10/Untitled-19-683x1024.jpg",
     name: "Fashion style",
     price: 80.99
@@ -176,7 +188,7 @@ const stockData = [
   },
   {
     id: "o3ahe30e-d98b-11e8-9y8b-f2801f1b9fd1",
-    category: "Chaussures",
+    category: "Shoes",
     cover: "https://www.ledetaillant.biz/wp-content/uploads/2021/01/Paire-monsieur-en-cuir-deux-tons-trois-boucles.png",
     name: "Fashion style",
     price: 19.96
@@ -184,13 +196,13 @@ const stockData = [
 ]
 
 // make an array of different category
-const arrCategory = Object.keys(stockData.map((item) => item.category).reduce((a, categ)=>{
+const arrCategory = Object.keys(stockData.map((item) => item.category).reduce((a, categ) => {
   a[categ] = a[categ] + 1 || 1;
   return a
 }, {}));
 
 // An object with category as keys and occurence number as value 
-let dataItems = stockData.map((item) => item.category).reduce((a, categ)=>{
+let dataItems = stockData.map((item) => item.category).reduce((a, categ) => {
   a[categ] = a[categ] + 1 || 1;
   return a
 }, {})
@@ -199,7 +211,7 @@ let dataItems = stockData.map((item) => item.category).reduce((a, categ)=>{
 arrCategory.map(categ => dataItems[categ] = []);
 
 // group items based on their category
-stockData.map((item) => dataItems[item.category].push(item) );
+stockData.map((item) => dataItems[item.category].push(item));
 
 
 
