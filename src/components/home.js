dfc1fc2ef841795e6929bf15c0d2e3b1a71dda3f
Messa Ismael <messaismael01@gmail.com>
2021-02-19 15:27:20 +0100

    improve promotion item in the home view

--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,7 @@ import '../css/home.scss'
 
 function Home() {
     return (
-        <div className='container home' >
+        <div className='container-md home' >
             <div className='row'>
                 {
                     homeData['section1'].map((item, i) => {
@@ -31,34 +31,37 @@ function Home() {
             <div className='all-categories'>
                 <NavLink to={'/shop-categories'}> All Categories </NavLink>
             </div>
-            <div className='row'>
-                <div className='col-12 col-md-6'>
-                    <p> New </p>
-                    <p> {homeData['section2'].name}</p>
-                    <div className='countdown mb'>
-                        <div className='date-item'>
-                            <div className='days mb-2'>00</div>
-                            <div className='days-label'>DAYS</div>
+            <div className='row promotion'>
+                <div className='col-12 col-md-6 mb-5'>
+                    <div className="rounded bg-light pb-5 position-relative">
+                        <span className='product-badge'>LIMITED OFFER</span>
+                        <h3 className="pt-5"> New </h3>
+                        <h1> {homeData['section2'].name}</h1>
+                        <div className='countdown'>
+                            <div className='date-item'>
+                                <div className='days mb-2'>00</div>
+                                <div className='days-label'>DAYS</div>
+                            </div>
+                            <div className='date-item'>
+                                <div className='hours mb-2'>00</div>
+                                <div className='hours-label'>HOURS</div>
+                            </div>
+                            <div className='date-item'>
+                                <div className='mins mb-2'>00</div>
+                                <div className='mins-label'>MINS</div>
+                            </div>
+                            <div className='date-item'>
+                                <div className='secs mb-2'>00</div>
+                                <div className='secs-label'>SECS</div>
+                            </div>
                         </div>
-                        <div className='date-item'>
-                            <div className='hours mb-2'>00</div>
-                            <div className='hours-label'>HOURS</div>
+                        <div className='btn-container' >
+                            <Button className="offer-btn" variant={"primary"} >View Offers</Button>
                         </div>
-                        <div className='date-item'>
-                            <div className='mins mb-2'>00</div>
-                            <div className='mins-label'>MINS</div>
-                        </div>
-                        <div className='date-item'>
-                            <div className='secs mb-2'>00</div>
-                            <div className='secs-label'>SECS</div>
-                        </div>
-                    </div>
-                    <div className='btn-container' >
-                        <Button className="offer-btn" variant={"primary"} >View Offers</Button>
                     </div>
                 </div>
-                <div className='col-12 col-md-6 img-container'>
-                    <div className='img-cover' style={{ "backgroundImage": `url(https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80)` }}>
+                <div className='col-12 col-md-6 img-container mb-5' style={{ minHeight: "270px" }}>
+                        <div className='img-cover rounded' style={{ "backgroundImage": `url(https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80)` }}>
                     </div>
                 </div>
             </div>
