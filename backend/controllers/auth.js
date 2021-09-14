2d478a3e918d5b063150985280ec289b93d6ac5b
messaismael <messaismael01@gmail.com>
2021-09-14 20:41:36 +0100

    improve register implementation server side

---
+++ backend/controllers/auth.js
@@ -0,0 +1,18 @@
+/**
+ * @author messaismael
+ * @since 0.0.1
+*/
+const AuthService = require("../services/auth")
+const status = require("http-status");
+const Util = require("../services/util")
+
+exports.register = async (req, res) => {
+  try{
+    const createdUser = await AuthService.register(req.body);
+    console.log(`User created widh the id ${createdUser._id}.`);
+    res.status(status.OK).json({ success: true, data: createdUser });
+  }catch (e){
+    Util.error(e);
+    res.status(status.BAD_REQUEST).json(e);
+  }
+}
