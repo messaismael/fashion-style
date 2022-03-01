042acc47258b4ac0305241ce874e39967858f519
Messa Ismael <messaismael01@gmail.com>
2022-03-01 20:21:24 +0100

    fix structures

--- frontend/src/config/constants.ts
+++
@@ -1,5 +0,0 @@
-const SERVER_BASE_URL = process.env.SERVER_BASE_URL;
-
-export {
-  SERVER_BASE_URL
-}
--- frontend/src/services/auth/auth.constant.ts
+++
@@ -1,4 +0,0 @@
-export const TOKEN = 'FS_TOKEN';
-export const USERNAME = 'FS_CURRENT_NAME';
-export const CURRENT_USER = 'FS_CURRENT_USER';
-//export const HS_PREVIOUS_URL = 'FS_PREVIOUS_URL';
